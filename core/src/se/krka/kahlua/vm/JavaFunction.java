315747e449cef27f42eb6b9e68fe39ac837f2563
Kristofer Karlsson <krka@krka-c400.(none)>
2010-06-03 22:19:43 +0200

    updated javadoc

--- core/src/se/krka/kahlua/vm/JavaFunction.java
+++ core/src/se/krka/kahlua/vm/JavaFunction.java
@@ -25,16 +25,16 @@ package se.krka.kahlua.vm;
 
 public interface JavaFunction {
 	/**
-	 * General contract<br>
+	 * General contract.<br>
 	 * <br>
 	 *  Input:<br>
 	 *  callFrame = the frame that contains all the arguments, and where all the results should be put.<br> 
-	 *  nArgs = number of function arguments<br>
+	 *  nArguments = number of function arguments<br>
 	 *  callFrame.get(i) = an argument (0 <= i < nArgs)<br> 
 	 *  
 	 * @param callFrame - the current callframe for the function 
 	 * @param nArguments - number of function arguments 
-	 * @return N - number of return values. The N top objects on the stack are considered the return values 
+	 * @return N - number of return values. The top N objects on the stack are considered the return values 
 	 */
 	public abstract int call(LuaCallFrame callFrame, int nArguments);
 }
