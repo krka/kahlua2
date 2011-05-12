301143e5e7139879a6f4a8761bc832e9827440a0
thehiflyer <per.malmen@gmail.com>
2011-05-12 23:15:16 +0200

    Loadfile will now return nil and an error message when the file can't be found

--- j2se/test/se/krka/kahlua/require/LoadfileTest.java
+++ j2se/test/se/krka/kahlua/require/LoadfileTest.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2011 Kristofer Karlsson <kristofer.karlsson@gmail.com>
+Copyright (c) 2011 Per Malmén <per.malmen@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
